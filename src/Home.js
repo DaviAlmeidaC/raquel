d2de13ff8e5950338a5763d0631d4d7959385fe1
Davi <nxsskill@gmail.com>
2025-06-16 03:21:27 -0400

    Fix: Removendo importação duplicada de useNavigate

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';  // Somente esta importação
 import './Home.css';
 
 export default function Home() {
@@ -25,40 +25,6 @@ export default function Home() {
 
       <img
         src="/image 4.png"
-        alt="Carro"
-        className="carro-img"
-      />
-    </div>
-  );
-}
-
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
-
-export default function Home() {
-  const navigate = useNavigate();
-
-  return (
-    <div className="home-container">
-      <h2 className="home-title">
-        Encontre Um Lugar<br/>Para Estacionar
-      </h2>
-
-      <button
-        className="achar-vaga-btn"
-        onClick={() => navigate('/vagas')}
-      >
-        <img
-          src="/carro-icon.png"
-          alt="Ícone de carro"
-          className="icon-carro"
-        />
-        <span className="btn-text">achar vaga</span>
-      </button>
-
-      <img
-        src="/carro.png"
         alt="Carro"
         className="carro-img"
       />
